49b5aebbe134807f28aa0449b0604d9b4d26c4e5
Hugo <hugopessanha22@gmail.com>
2022-05-12 20:52:58 -0300

    vuejs

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold" :styles="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    export default defineComponent({
+        // eslint-disable-next-line vue/multi-word-component-names
+        name: 'Box',
+        data() {
+            return{
+                estilos:{
+                     backgroundColor: '#FAF0CA',
+                }
+            }
+        }
+    });
+
+</script>
